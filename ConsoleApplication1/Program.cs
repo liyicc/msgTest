4c2680a19e4469ed0097e5caa1eaf354b7676884
liyicc@163.com <liyicc@163.com>
2017-01-15 20:33:38 +0800

    change1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,7 +9,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!"); 
+            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("change!"); 
         }
     }
 }
