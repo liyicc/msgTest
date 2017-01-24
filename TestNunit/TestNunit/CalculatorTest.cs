ca44560dd7d1950a8ce0a9ffe7e8e86bec4ba9bf
liyicc@163.com <liyicc@163.com>
2017-01-24 14:01:28 +0800

    mess

--- TestNunit/TestNunit/CalculatorTest.cs
+++ TestNunit/TestNunit/CalculatorTest.cs
@@ -33,8 +33,8 @@ namespace TestNunit
         public void TestMul()
         {
             Class1 cal = new Class1();
-            int expected = 49;
-            int actual = cal.Mul(7, 7);
+            int expected = 24;
+            int actual = cal.Mul(3, 8);
             Assert.AreEqual(expected, actual);
         }
 
